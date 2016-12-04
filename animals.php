409ae7f34b09d15565a7fe46fdb5be850b5f6bd1
Tatartrichilo <sugmugrick@gmail.com>
2016-12-04 15:01:51 -0500

    Update and rename animals.html to animals.php

--- animals.php
+++ animals.php
@@ -6,7 +6,7 @@
       	<a class="navbar-brand" href="index.html"><img src="oasis logo.png"/></a>
     	</div>
         <ul class="nav navbar-nav navbar-right" style="font-size: 24px; margin-top: 35px !important;">
-	<li class="active"><a href="animals.html">Animals</a></li>
+	<li class="active"><a href="animals.php">Animals</a></li>
 	<li><a href="shows.html">Shows</a></li>
 	<li><a href="tours.html">Tours</a></li>
 	<li><a href="vlogin.html">Visitor Login</a></li>
@@ -16,10 +16,40 @@
 	  </nav>
 </head>
   <body>
-    <div class="jumbotron home_jumbotron" style="background-color: #FFFFFF; padding: 5px;">
-	<div><center><img src="aquarium.jpg" class="img-fluid" style="max-width: 99%; max-height: 99%;"></center>
-      	</div>
-    </div>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "aqua";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM species";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         $speciesname = $row['species'];
+	 $tankname = $row['tank_id'];
+	 $size = $row['size'];
+	 $diet = $row['diet'];
+	 $lifespan = $row['lifespan'];
+	 echo "Species: " . $speciesname . "<br>";
+	 echo "Tank: " . $tankname . "<br>";
+	 echo "Size: " . $size . "<br>";
+	 echo "Diet: " . $diet . "<br>";
+	 echo "<br>";
+     }
+} else {
+     echo "0 results";
+}
+?>
 
     	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
